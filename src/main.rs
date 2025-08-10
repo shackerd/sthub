05d4c240094f46e9f6a661c0073daec1cc8d2e89
shackerd <r.csgnl@pm.me>
2025-08-10 23:08:57 +0200

    add basic static file support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use actix_web::{App, HttpServer, Responder, web};
+use actix_files::NamedFile;
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
 use serde_json::{Map, Value};
 use std::collections::BTreeMap;
 use std::env;
+use std::path::PathBuf;
 
 fn build_tree(vars: Vec<(String, String)>, prefix: &str) -> Value {
     let mut root = BTreeMap::new();
@@ -43,6 +45,20 @@ fn insert_nested(map: &mut BTreeMap<String, Value>, parts: &[&str], value: Strin
     }
 }
 
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
+
+async fn doc_index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    // root directory is not the current working directory
+    // so we need to specify the path correctly
+    let req_path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    let mut path = PathBuf::from("./assets/");
+    path.push(req_path);
+    Ok(NamedFile::open(path)?)
+}
+
 async fn env_api() -> impl Responder {
     let vars: Vec<(String, String)> = env::vars().collect();
     let tree = build_tree(vars, "MYAPI__");
@@ -51,8 +67,13 @@ async fn env_api() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/env", web::get().to(env_api)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/env", web::get().to(env_api))
+            .route("/doc/{filename:.*}", web::get().to(doc_index))
+            .route("/{filename:.*}", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
